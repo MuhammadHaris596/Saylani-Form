4e5d331248307d20df094ee36ef496a1c245fda5
Muhammad Haris <134528490+MuhammadHaris596@users.noreply.github.com>
2024-11-10 16:42:17 +0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,92 +1,54 @@
 
-
-var country = document.getElementById("country-select")
-console.log(country)
-
-
-var city = document.getElementById("city-select")
-console.log(city)
-
-
-
-var course = document.getElementById("course-select")
-console.log(course)
-
-
-var proficiency = document.getElementById("proficiency-select")
-console.log(proficiency)
-
-
-
-
-var fullname = document.getElementById("full-name")
-console.log(fullname)
-
-
-
-var fathername = document.getElementById("father-name")
-console.log(fathername)
-
-
-var Email = document.getElementById("email")
-console.log(Email)
-
-
-
-var Phone = document.getElementById("Phone")
-console.log(Phone)
-
-
-
-var cnic = document.getElementById("cnic")
-console.log(cnic)
-
-
-
-
-var fatherCnic = document.getElementById("f-Cnic")
-console.log(fatherCnic)
-
-
-
-
-var DOB = document.getElementById("DOB")
-console.log(DOB)
-
-
-
-
-var Gender = document.getElementById("gender")
-console.log(Gender)
-
-
-
-var address = document.getElementById("address")
-console.log(address)
-
-
-
-
-
-var Qualification = document.getElementById("qualification")
-console.log(Qualification)
-
-
-
-
-var Ownlaptop = document.getElementById("ownLaptop")
-console.log(Ownlaptop)
-
-
-var submit = document.getElementById("submit")
-console.log(submit)
-
-
-
 function Submit() {
-    
+
     alert("Form has been submitted!");
     
+var country = document.getElementById("country-select").value
+var city = document.getElementById("city-select").value
+var course = document.getElementById("course-select").value
+
+var proficiency = document.getElementById("proficiency-select").value
+
+var fullname = document.getElementById("full-name").value
+
+var fathername = document.getElementById("father-name").value
+
+var Email = document.getElementById("email").value
+
+var Phone = document.getElementById("Phone").value
+
+var cnic = document.getElementById("cnic").value
+
+var fatherCnic = document.getElementById("f-Cnic").value
+
+var DOB = document.getElementById("DOB").value
+
+var Gender = document.getElementById("gender").value
+var address = document.getElementById("address").value
+
+var Qualification = document.getElementById("qualification").value
+var Ownlaptop = document.getElementById("ownLaptop").value
+
+
+    console.log(country)
+    console.log(city)
+    console.log(course)
+    console.log(proficiency)
+    console.log(Ownlaptop)
+    console.log(Qualification)
+    console.log(address)
+    console.log(Gender)
+    console.log(fatherCnic)
+    console.log(fullname)
+    console.log(DOB)
+    console.log(cnic)
+    console.log(Phone)
+    console.log(Email)
+    console.log(fathername)
+
+
+    
+
 }
 
 
@@ -101,41 +63,3 @@ function Submit() {
 
 
 
-// h1.style.color= "Darkblue"
-// h1.textContent += " Haris"
-
-// console.log(document)
-
-
-/*var key = prompt("Enter the key")
-var value = prompt("Enter the value")
-localStorage.setItem(key,value)
-// console.log(`The key is ${key} and the value is ${localStorage.getItem(value)}`)
-
-
-if ( value == "red" || value == "black" ){
-    localStorage.removeItem(key,value);
-}
-
-if ( key === "0" ){
-    localStorage.clear();
-}   
-
-*/
-
-// document.body.style.backgroundColor = "crimson"
-
-
-// document.title = "My-Webpage"
-
-// // document.URL = "www.haris.com"
-
-// var h1 = document.querySelector("h1")
-// h1.style.textAlign ="center"
-// h1.style.display ="flex"
-// h1.style.justifyContent ="center"
-// h1.style.alignItems ="center"
-// h1.style.height = "100px";
-// // h1.style.margin = "90px 0px"
-// h1.style.padding = "30px 0px"
-// h1.style.backgroundColor = "crimson"
